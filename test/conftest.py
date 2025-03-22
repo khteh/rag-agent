340681cccb34e22f75cb4cc47625876af0621092
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-22 17:09:18 +0800

    Import after initialize vertexai

--- test/conftest.py
+++ test/conftest.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from dotenv import load_dotenv
 from os.path import dirname, join, abspath
 from langchain_core.runnables import RunnableConfig
-from rag_agent.EmailRAG import EmailRAG
 sys.path.insert(0, abspath(join(dirname(__file__), '../src')))
 pytest_plugins = ('pytest_asyncio',)
 load_dotenv()
@@ -13,6 +12,7 @@ def rag():
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
     vertexai.init(project=os.environ.get("GOOGLE_CLOUD_PROJECT"), location=os.environ.get("GOOGLE_CLOUD_LOCATION"))
     config = RunnableConfig(run_name="Email RAG", thread_id=datetime.now())
+    from rag_agent.EmailRAG import EmailRAG
     return EmailRAG(config)
 
 def pytest_generate_tests(metafunc):
