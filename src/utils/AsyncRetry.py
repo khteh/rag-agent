3b879cbfe6709f721de9ddea7584c13ddb218fae
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-19 19:59:17 +0800

    Add reference and explaination of async_retry decorator with input parameters function

--- src/utils/AsyncRetry.py
+++ src/utils/AsyncRetry.py
@@ -1,5 +1,11 @@
 import asyncio, logging, functools
 
+"""
+https://realpython.com/primer-on-python-decorators/#defining-decorators-with-arguments
+(1) Defining wrapper() as an inner function means that async_retry() will refer to a function object, wrapper.
+    Now, you need to add parentheses when setting up the decorator, as in @async_retry(). This is necessary in order to add arguments.
+(2) The max_retries and delay arguments are seemingly not used in async_retry() itself. But by passing max_retries and delay, a closure is created where the value of max_retries and delay are stored until wrapper() uses it later.
+"""
 def async_retry(max_retries: int=3, delay: int=0):
     def decorator(func):
         @functools.wraps(func)
