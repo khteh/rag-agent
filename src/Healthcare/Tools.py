e84123c79dcb6b783f35ffcd3b064556d7d3b155
Kok How, Teh <funcoolgeek@gmail.com>
2026-01-17 14:05:48 +0800

    Neo4J vector and cypher exception handling

--- src/Healthcare/Tools.py
+++ src/Healthcare/Tools.py
@@ -10,6 +10,7 @@ from langchain_community.vectorstores import Neo4jVector
 from langchain_ollama import OllamaEmbeddings
 from langchain_classic.chains import create_retrieval_chain
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from neo4j.exceptions import CypherSyntaxError
 from src.common.Configuration import Configuration
 from src.config import config as appconfig
 # For VertexAI, use VertexAIEmbeddings, model="text-embedding-005"; "gemini-2.0-flash" model_provider="google_genai"
@@ -26,44 +27,48 @@ async def HealthcareReview(
     query: str, *, config: Annotated[RunnableConfig, InjectedToolArg]
 ) -> dict[str, float]:
     logging.info(f"\n=== HealthcareReview ===")
-    neo4j_vector_index = Neo4jVector.from_existing_graph(
-        embedding = OllamaEmbeddings(model=appconfig.EMBEDDING_MODEL, base_url=appconfig.BASE_URI, num_ctx=8192, num_gpu=1, temperature=0),
-        url = appconfig.NEO4J_URI,
-        username = appconfig.NEO4J_USERNAME,
-        password = appconfig.NEO4J_PASSWORD,
-        index_name = "reviews",
-        node_label = "Review",
-        text_node_properties=[
-            "physician_name",
-            "patient_name",
-            "text",
-            "hospital_name",
-        ],
-        embedding_node_property="embedding",
-    )
-    configuration = Configuration.from_runnable_config(config)
-    review_system_prompt = SystemMessagePromptTemplate(
-        prompt=PromptTemplate(input_variables=["context"], template=configuration.review_prompt)
-    )
-    review_human_prompt = HumanMessagePromptTemplate(
-        prompt=PromptTemplate(input_variables=["input"], template="{input}")
-    )
-    messages = [review_system_prompt, review_human_prompt]
-    review_prompt = ChatPromptTemplate(
-        input_variables=["context", "input"], messages=messages
-    )
-    if appconfig.BASE_URI:
-        llm = init_chat_model(appconfig.LLM_RAG_MODEL, model_provider=appconfig.MODEL_PROVIDER, base_url=appconfig.BASE_URI, streaming=True, temperature=0)
-    else:
-        llm = init_chat_model(appconfig.LLM_RAG_MODEL, model_provider=appconfig.MODEL_PROVIDER, streaming=True, temperature=0)
-    #create_stuff_documents_chain():
-    #    prompt: Prompt template. Must contain input variable `"context"` (override by
-    #    setting document_variable), which will be used for passing in the formatted
-    #    documents.
-    question_answer_chain = create_stuff_documents_chain(llm, review_prompt)
-    reviews_vector_chain = create_retrieval_chain(neo4j_vector_index.as_retriever(k=3), question_answer_chain)
-    logging.debug(f"query: {query}")
-    return await reviews_vector_chain.ainvoke({"input": query})
+    try:
+        neo4j_vector_index = Neo4jVector.from_existing_graph(
+            embedding = OllamaEmbeddings(model=appconfig.EMBEDDING_MODEL, base_url=appconfig.BASE_URI, num_ctx=8192, num_gpu=1, temperature=0),
+            url = appconfig.NEO4J_URI,
+            username = appconfig.NEO4J_USERNAME,
+            password = appconfig.NEO4J_PASSWORD,
+            index_name = "reviews",
+            node_label = "Review",
+            text_node_properties=[
+                "physician_name",
+                "patient_name",
+                "text",
+                "hospital_name",
+            ],
+            embedding_node_property="embedding",
+        )
+        configuration = Configuration.from_runnable_config(config)
+        review_system_prompt = SystemMessagePromptTemplate(
+            prompt=PromptTemplate(input_variables=["context"], template=configuration.review_prompt)
+        )
+        review_human_prompt = HumanMessagePromptTemplate(
+            prompt=PromptTemplate(input_variables=["input"], template="{input}")
+        )
+        messages = [review_system_prompt, review_human_prompt]
+        review_prompt = ChatPromptTemplate(
+            input_variables=["context", "input"], messages=messages
+        )
+        if appconfig.BASE_URI:
+            llm = init_chat_model(appconfig.LLM_RAG_MODEL, model_provider=appconfig.MODEL_PROVIDER, base_url=appconfig.BASE_URI, streaming=True, temperature=0)
+        else:
+            llm = init_chat_model(appconfig.LLM_RAG_MODEL, model_provider=appconfig.MODEL_PROVIDER, streaming=True, temperature=0)
+        #create_stuff_documents_chain():
+        #    prompt: Prompt template. Must contain input variable `"context"` (override by
+        #    setting document_variable), which will be used for passing in the formatted
+        #    documents.
+        question_answer_chain = create_stuff_documents_chain(llm, review_prompt)
+        reviews_vector_chain = create_retrieval_chain(neo4j_vector_index.as_retriever(k=3), question_answer_chain)
+        logging.debug(f"query: {query}")
+        return await reviews_vector_chain.ainvoke({"input": query})
+    except Exception as e:
+        logging.exception(f"{HealthcareReview.__name__} exception! {str(e)}, repr: {repr(e)}")
+        return repr(e)
 
 @tool(description="""Useful for answering questions about patients,
         physicians, hospitals, insurance payers, patient review
@@ -80,26 +85,33 @@ async def HealthcareCypher(
         username = appconfig.NEO4J_USERNAME,
         password = appconfig.NEO4J_PASSWORD,
     ) as graph:
-        configuration = Configuration.from_runnable_config(config)
-        cypher_generation_prompt = PromptTemplate(
-            input_variables=["schema", "question"], template=configuration.cypher_generation_prompt
-        )
-        qa_generation_prompt = PromptTemplate(
-            input_variables=["context", "question"], template=configuration.qa_generation_prompt
-        )
-        if appconfig.BASE_URI:
-            llm = init_chat_model(appconfig.LLM_RAG_MODEL, model_provider=appconfig.MODEL_PROVIDER, base_url=appconfig.BASE_URI, streaming=True, temperature=0)
-        else:
-            llm = init_chat_model(appconfig.LLM_RAG_MODEL, model_provider=appconfig.MODEL_PROVIDER, streaming=True, temperature=0)
-        hospital_cypher_chain = GraphCypherQAChain.from_llm( # 'GraphCypherQAChain' object does not support the context manager protocol"
-            cypher_llm = llm,
-            qa_llm = llm,
-            graph = graph,
-            verbose = appconfig.LOGLEVEL == "DEBUG", # Whether intermediate steps your chain performs should be printed.
-            qa_prompt = qa_generation_prompt,
-            cypher_prompt = cypher_generation_prompt,
-            validate_cypher = True,
-            top_k = 100,
-            allow_dangerous_requests = True # https://python.langchain.com/docs/security/
-        )
-        return await hospital_cypher_chain.ainvoke(query)
+        try:
+            configuration = Configuration.from_runnable_config(config)
+            cypher_generation_prompt = PromptTemplate(
+                input_variables=["schema", "question"], template=configuration.cypher_generation_prompt
+            )
+            qa_generation_prompt = PromptTemplate(
+                input_variables=["context", "question"], template=configuration.qa_generation_prompt
+            )
+            if appconfig.BASE_URI:
+                llm = init_chat_model(appconfig.LLM_RAG_MODEL, model_provider=appconfig.MODEL_PROVIDER, base_url=appconfig.BASE_URI, streaming=True, temperature=0)
+            else:
+                llm = init_chat_model(appconfig.LLM_RAG_MODEL, model_provider=appconfig.MODEL_PROVIDER, streaming=True, temperature=0)
+            hospital_cypher_chain = GraphCypherQAChain.from_llm( # 'GraphCypherQAChain' object does not support the context manager protocol"
+                cypher_llm = llm,
+                qa_llm = llm,
+                graph = graph,
+                verbose = appconfig.LOGLEVEL == "DEBUG", # Whether intermediate steps your chain performs should be printed.
+                qa_prompt = qa_generation_prompt,
+                cypher_prompt = cypher_generation_prompt,
+                validate_cypher = True,
+                top_k = 100,
+                allow_dangerous_requests = True # https://python.langchain.com/docs/security/
+            )
+            return await hospital_cypher_chain.ainvoke(query)
+        except CypherSyntaxError as syntax_error:
+            logging.exception(f"{HealthcareCypher.__name__} CypherSyntaxError: {syntax_error}, repr: {repr(e)}")
+            return syntax_error.message
+        except Exception as e:
+            logging.exception(f"{HealthcareCypher.__name__} exception! {str(e)}, repr: {repr(e)}")
+            return repr(e)
