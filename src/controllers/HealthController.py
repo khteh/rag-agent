5e73e6bab8eac3840ea13464f6405bb0d65e0fd5
Kok How, Teh <funcoolgeek@gmail.com>
2026-01-25 10:28:20 +0800

    Use current_app db_pool for healthcheck endpoints

--- src/controllers/HealthController.py
+++ src/controllers/HealthController.py
@@ -1,5 +1,4 @@
 import logging
-from psycopg_pool import AsyncConnectionPool, ConnectionPool
 from quart import (
     Blueprint,
     Response,
@@ -9,34 +8,23 @@ from quart import (
     render_template,
     session
 )
-from src.config import config
 health_api = Blueprint("health", __name__)
-
 async def _healthcheck() -> ResponseReturnValue:
     try:
-        connection_kwargs = {
-            "autocommit": True,
-            "prepare_threshold": 0,
-        }
-        async with AsyncConnectionPool(
-            conninfo = config.POSTGRESQL_DATABASE_URI,
-            max_size = config.DB_MAX_CONNECTIONS,
-            kwargs = connection_kwargs,
-        ) as pool:
-            # Check if the checkpoints table exists
-            async with pool.connection() as conn:
-                async with conn.cursor() as cur:
-                    try:
-                        await cur.execute("""
-                            SELECT EXISTS (
-                                SELECT FROM information_schema.tables 
-                                WHERE  table_schema = 'public'
-                                AND    table_name   = 'checkpoints'
-                            );
-                        """)
-                    except Exception as e:
-                        logging.exception(f"Error checking for checkpoints table: Exception: {e}")
-                        raise e
+        # Check if the checkpoints table exists
+        async with current_app.db_pool.connection() as conn:
+            async with conn.cursor() as cur:
+                try:
+                    await cur.execute("""
+                        SELECT EXISTS (
+                            SELECT FROM information_schema.tables 
+                            WHERE  table_schema = 'public'
+                            AND    table_name   = 'checkpoints'
+                        );
+                    """)
+                except Exception as e:
+                    logging.exception(f"Error checking for checkpoints table: Exception: {e}")
+                    raise e
         #logging.debug(f"config.CHROMA_URI: {config.CHROMA_URI}")
         #client = chromadb.HttpClient(host=config.CHROMA_URI, port=80, headers={"X-Chroma-Token": config.CHROMA_TOKEN})
         #client.heartbeat()  # this should work with or without authentication - it is a public endpoint
